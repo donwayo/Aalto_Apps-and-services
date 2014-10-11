d73bc4d5f8ae4eb72b1e04704f7e69c59276555a
Eduardo Castellanos <ecn@kth.se>
2014-10-11 21:14:38 +0300

    Changed to command line. Cleaned up a little code, but still need to fix more.

--- assignment_1/src/main.py
+++ assignment_1/src/main.py
@@ -1,14 +1,14 @@
-
 import asyncore
 import socket
+import sys
 from settings import *
 from messages import *
 
-def log(s):
-    print("[{0}] {1}".format(time.ctime(),s))
+def log(s,l):
+    if l <= LOG_LVL:
+        print("[{0}] {1}".format(time.ctime(),s))
 
 class P2PMain():
-    ControlClients = {}
     Peers = {}
     HostIP = struct.unpack("!I",socket.inet_aton(socket.gethostbyname(socket.gethostname())))[0]
     ConnectionCount = 0
@@ -38,59 +38,27 @@ class P2PMain():
             self.Peers[i].close()
             del self.Peers[i]
 
-    def controlConnect(self, client):
-        self.ControlClients[self.ConnectionCount] = client
-        self.ConnectionCount = self.ConnectionCount + 1
-
-    def controlClose(self, client):
-        idx = -1
-        for i in self.ControlClients:
-            if self.ControlClients[i] == client:
-                idx = i
-        self.controlDelete(idx)
-
-    def controlDelete(self, idx):
-        if idx >= 0 and (idx in self.ControlClients):
-            self.ControlClients[i].close()
-            del self.Peers[i]
-
     def shutDown(self):
         for i in p2p.Peers:
             self.Peers[i].close()
-
-        for i in p2p.ControlClients:
-            self.ControlClients[i].close()
+        cmdline.close()
         
-        main.close()
-
     def __str__(self):
-       return "\nControl connections: {0}\nPeer connections: {1}\n{2}\n".format(len(self.ControlClients), len(self.Peers), self.Peers.keys())
+       return "\nPeer connections: {1}\n".format(len(self.Peers), self.Peers.keys())
 
 
-# Control server
-class MainServerSocket(asyncore.dispatcher):
-    def __init__(self, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.bind(('',port))
-        self.listen(5)
+# Command line control
+class CmdlineClient(asyncore.file_dispatcher):
+    def __init__(self, file):
+        asyncore.file_dispatcher.__init__(self, file)
 
-    def handle_accept(self):
-        newSocket, address = self.accept()
-        log("Connected from {0}:{1}.".format(address[0], address[1]))
-        cclient = SecondaryServerSocket(newSocket)
-        p2p.controlConnect(cclient)
-
-# Control connection
-class SecondaryServerSocket(asyncore.dispatcher_with_send):
     def handle_read(self):
-        receivedData = self.recv(8192)
+        receivedData = self.recv(1024)
         if receivedData: 
             receivedData = receivedData.rstrip()
             # Search command is 's' followed by the search string.
             if receivedData[0] == 's':
-                log('Search {0}'.format(receivedData[1:]))
+                log('Search {0}'.format(receivedData[1:]), 2)
                 self.send('Searching {0}\n'.format(receivedData[1:]))
 
             # Join network is 'j' followed by 'b' for the bootstrap node or an IP address.
@@ -107,27 +75,23 @@ class SecondaryServerSocket(asyncore.dispatcher_with_send):
             # Send Bye
             elif receivedData[0] == 'b' and len(receivedData) > 1:
                 cId = int(receivedData[1:])
+                log('Got request to send Bye message to connection {0}'.format(cId), 1)
                 p2p.sendBye(cId)
-                log('Sending bye to {0}'.format(cId))
-
+                
             # Display information
             elif receivedData[0] == 'i':
                 if len(receivedData[1:]) > 0:
                     self.send("O: {0}\n".format(eval(receivedData[1:])))
                 else:
-                    self.send("{0}".format(p2p))
+                    self.send("{0}\n".format(p2p))
             # Close down.
             elif receivedData[0] == 'q':
-                log("Closing")
+                log("Closing down.",1)
                 self.send('Bye\n')
                 p2p.shutDown()
 
         else: 
             self.close()
-
-    def handle_close(self):
-        log("Disconnected from {0}.".format(self.getpeername()))
-        self.close()
 
 # P2P Connections
 class P2PConnection(asyncore.dispatcher):
@@ -146,12 +110,13 @@ class P2PConnection(asyncore.dispatcher):
         self.out_buffer = message.GetBytes()
 
         self.JoinMessageId = message.MessageId
-        log("Attempting to join with Message:\n {0}".format(message))
+        log("Attempting to join with Message:\n {0}".format(message),2)
 
     def bye(self):
         msg = ByeMessage(self.getIP())
         self.out_buffer = msg.GetBytes()
-        log("Sending Bye message: \n{0}".format(msg))
+        log("Sending Bye message to {0}".format(self.getpeername()[0]),1)
+        log("{0}".format(msg),2)
         
     def handle_write(self):
         sent = self.send(self.out_buffer)
@@ -177,33 +142,35 @@ class P2PConnection(asyncore.dispatcher):
         msg = ParseData(receivedData)
         if msg:
             self.handle_message(msg)
-        else:
-            log('Got trash')
+            log('{0}'.format(msg), 3)
+        elif len(receivedData) > 0:
+            log('Got trash', 1)
 
     def handle_message(self, msg):
         if msg.Type == P2PMessage.MSG_JOIN:
             if msg.MessageId == self.JoinMessageId and \
             not msg.Request:
                 self.Joined = True
-                log("Joined successfully @ {0}".format(msg.GetSenderIP()))
+                log("Joined successfully @ {0}".format(msg.GetSenderIP()), 1)
                 self.JoinTime = time.time()
         elif msg.Type == P2PMessage.MSG_BYE:
-            log("Got BYE message. Closing connection with {0}:{1}".format(self.getpeername(), self.getPort()))
+            log("Got BYE message. Closing connection with {0}:{1}".format(self.getpeername(), self.getPort()),1)
             p2p.leave(self)
         else:
-            log("Unhandled message: \n{0}".format(msg))
+            log("Unhandled message from {0}:{1}".format(self.getpeername()[0], self.getpeername()[1]),1)
+            log("{0}".format(msg), 2)
 
     def handle_close(self):
-        log("Disconnected from {0}:{1}.".format(self.getpeername()[0], self.getpeername()[1]))
-        self.close()
+        log("Disconnected from {0}:{1}.".format(self.getpeername()[0], self.getpeername()[1]),1)
+        p2p.leave(self)
 
     def handle_connect(self):
-        log("Connected to {0}:{1}.".format(self.getpeername()[0], self.getpeername()[1] ))
+        log("Connected to {0}:{1}.".format(self.getpeername()[0], self.getpeername()[1] ),1)
 
     def __str__(self):
-        return "Peer connection to {0}:{1}\n\tAlive for {2}".format(self.getpeername()[0], self.getpeername()[1], time.time()-self.JoinTime)
+        return "Peer connection to {0}:{1}\n\tAlive for {2}".format(self.getpeername()[0], self.getpeername()[1], time.time()-self.JoinTime,1)
 
 # P2P Server
 p2p = P2PMain()
-main = MainServerSocket(Q_PORT)
+cmdline = CmdlineClient(sys.stdin)
 asyncore.loop(1)

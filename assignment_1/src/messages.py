e0b980a158e5e8abe132007e63dd67fe87bd2ffc
Eduardo Castellanos <ecn@kth.se>
2014-10-20 15:38:31 +0300

    Merge

--- assignment_1/src/messages.py
+++ assignment_1/src/messages.py
@@ -101,8 +101,9 @@ def ParseData(data):
             elif header[2] == P2PMessage.MSG_QUERY:
                 msg = QueryMessage(0)
                 msg.FromData(header, payload)
-            #elif header[2] == P2PMessage.MSG_PONG:
-            #    print('unimplemented\n')
+            elif header[2] == P2PMessage.MSG_PONG:
+                msg = PongMessage(0)
+                msg.FromData(header, payload)
             elif header[2] == P2PMessage.MSG_BYE:
                 msg = ByeMessage(0)
                 msg.LoadHeader(header)
@@ -141,12 +142,17 @@ class PongMessage(P2PMessage):
         self.Type = P2PMessage.MSG_PONG
         self.SenderIP = ipaddr
 
-    def addEntry(self, ip, port):
+    def AddEntry(self, ip, port):
         Entries.append([ip, port])
+
+    def FromData(self, header, payload):
+        if len(header) == 8:
+            self.LoadHeader(header)
+
+            # Handle payload.
 
     def GetBytes(self):
         # Build entry payload. 
-        
         self.Payload = struct.pack('!HH', len(self.Entries), 0)
         for e in self.Entries:
             self.Payload += PongEntry.pack(e[0], e[1], 0)
